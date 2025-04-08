b60d2a853fbc1a3adba86b2ba12e30ee2e5ff3e4
thanush <thanush.learn@gmail.com>
2025-04-08 23:45:17 +0530

    Fixed the bug that didn'r create users and added a display board layout

--- script.js
+++ script.js
@@ -1,4 +1,92 @@
 
+let players = {
+    player1: { player: 1, name: "Player 1", symbol: "X" },
+    player2: { player: 2, name: "Player 2", symbol: "O" }
+};
+
+
+const DisplayFunctions = (function () {
+
+    let mainContainer = document.querySelector(".main-container");
+
+    function UserDialog() {
+
+        let getUser = 
+        `<dialog open id="input-dialog">
+            <form method="dialog" id="nameForm">
+                <label for="p1name">Enter Player 1 name</label>
+                <input type="text" name="p1name" id="p1name">
+                <br>
+                <label for="p2name">Enter Player 2 name</label>
+                <input type="text" name="p2name" id="p2name">
+                <br>
+                <input type="submit" id="enter-button">
+            </form>
+        </dialog>`;
+    
+        mainContainer.innerHTML = getUser;
+    
+        const enterBtn = document.getElementById("enter-button");
+
+        enterBtn.addEventListener("click", function (e) {
+            e.preventDefault(); 
+    
+            const p1name = document.getElementById("p1name").value.trim();
+            const p2name = document.getElementById("p2name").value.trim();
+    
+            players.player1.name = p1name;
+            players.player2.name = p2name;
+
+            document.getElementById("input-dialog").close();
+            console.log(players);
+        });
+    }
+
+
+    function DisplayBoard(){
+        mainContainer.innerHTML = "";
+
+        const displayHTML = `
+        <div class="display-container">
+            <h1>TicTacToe</h1>
+
+            <div class="player-header">
+                <div class="player">
+                    <p id="player-1-name">Player 1</p>
+                    <p id="player-1-score">0</p>
+                </div>
+                <div class="player">
+                    <p id="player-2-name">Player 2</p>
+                    <p id="player-2-score">0</p>
+                </div>
+            </div>
+
+            <div class="game-board">
+                <div class="box" id="r0c1"></div>
+                <div class="box" id="r0c2"></div>
+                <div class="box" id="r0c3"></div>
+                <div class="box" id="r1c1"></div>
+                <div class="box" id="r1c2"></div>
+                <div class="box" id="r1c3"></div>
+                <div class="box" id="r2c1"></div>
+                <div class="box" id="r2c2"></div>
+                <div class="box" id="r2c3"></div>
+            </div>
+        </div>
+        `
+
+        mainContainer.innerHTML = displayHTML;
+    }
+
+    return {
+        UserDialog,
+        players,
+        DisplayBoard
+    };
+})();
+
+
+
 const gameBoard = (function () {
     const allEqual = arr => arr.every(val => val !== "*" && val === arr[0]);
     let gameBoard = [
@@ -8,6 +96,7 @@ const gameBoard = (function () {
     ];
 
     function getGameBoard() {
+        //The console method
         for (let i = 0; i < 3; i++) {
             let rowString = "";
             for (let j = 0; j < 3; j++) {
@@ -15,16 +104,16 @@ const gameBoard = (function () {
             }
             console.log(rowString.trim());
         }
+
+        //Calling the display method
+        DisplayFunctions.DisplayBoard();
     }
 
 
-    function createPlayers(name1, name2) {
-
-        return {
-            player1: { player: 1, name: name1, symbol: "X" },
-            player2: { player: 2, name: name2, symbol: "O" }
-        };
+    function createPlayers() {
+        DisplayFunctions.UserDialog();
     }
+    
 
     function userPositionPrompt() {
         const row = parseInt(prompt("Enter row (0,1,2)"));
@@ -79,25 +168,26 @@ const gameBoard = (function () {
 })();
 
 
-
 const gameFlow = (function () {
-    //Step 1, create players
-    players = gameBoard.createPlayers("A", "B");
+    //Step 1, initialise player symbols
+    gameBoard.createPlayers();
+    console.log("players initialised successfully",players);
     //step 2, show the board
     gameBoard.getGameBoard();
     //step3, logic for switching user turns
-    let isGameOver = false;
-    currentTurn = players.player1;
-    while (isGameOver === false) {
-        let position = gameBoard.userPositionPrompt();
-        gameBoard.updateGameBoard(position.row, position.column, currentTurn.symbol)
-        if (gameBoard.checkWinner() === true) {
-            console.log(currentTurn.name + " Wins!");
-            isGameOver = true;
-        }
-        currentTurn = currentTurn === players.player1 ? players.player2 : players.player1;
-    }
+//     let isGameOver = false;
+//     currentTurn = players.player1;
+//     while (isGameOver === false) {
+//         let position = gameBoard.userPositionPrompt();
+//         gameBoard.updateGameBoard(position.row, position.column, currentTurn.symbol)
+//         if (gameBoard.checkWinner() === true) {
+//             console.log(currentTurn.name + " Wins!");
+//             isGameOver = true;
+//         }
+//         currentTurn = currentTurn === players.player1 ? players.player2 : players.player1;
+//     }
 })
+
 
 
 gameFlow();
